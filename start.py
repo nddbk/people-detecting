39a46466d1dc276e15e556291dcc7483cf8ffdb7
Dong Nguyen <ndaidong@gmail.com>
2018-01-26 14:33:53 +0700

    Implement sample

--- start.py
+++ start.py
@@ -1,4 +1,9 @@
+import numpy as np
+from imutils.object_detection import non_max_suppression
 import cv2
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
@@ -9,45 +14,46 @@ h = cap.get(4)
 mx = int(w - 400)
 my = int(h - 24)
 
-avg = None
-
 while(cap.isOpened()):
     ret, frame = cap.read()
 
-    if ret == False:
+    if ret is False:
         break
     
     k = cv2.waitKey(30) 
     if k == 27:
         break
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    rects, weights = hog.detectMultiScale(
+        frame,
+        winStride=(4, 4), 
+        padding=(8, 8), 
+        scale=1.05
+    )
+    for (x, y, w, h) in rects:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    if avg is None:
-        avg = gray.copy().astype("float")
-        continue
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
     
-    cv2.accumulateWeighted(gray, avg, 0.1)
-    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-    cv2.imshow('Frame', frameDelta)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    _, contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
     people_detected = 0
-
-    for c in contours:
-        if cv2.contourArea(c) < 4000:
-            continue
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
+    
+    for (xA, yA, xB, yB) in pick:
         people_detected += 1
+        cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
     text = 'People detected: ' + str(people_detected)
-    cv2.putText(frame, text, (mx, my), font, 1, (255, 255, 255), 1, cv2.LINE_AA) 
-    cv2.imshow('Frame',frame)
+    cv2.putText(
+        frame, 
+        text, 
+        (mx, my), 
+        font, 
+        1, 
+        (255, 255, 255), 
+        1, 
+        cv2.LINE_AA
+    )
+    cv2.imshow('Frame', frame)
 
 cap.release()
 cv2.destroyAllWindows()
