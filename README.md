784894a49bbe8e487057cad85ba076a071eb25da
Dong Nguyen <ndaidong@gmail.com>
2018-01-26 11:43:39 +0700

    Implement 1st time

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # people-detecting
 Detect people from video/camera using OpenCV
 
-This was created to try some algorighms for detecting people from video/camera. 
+
+# What
+
 
 ```
 git clone https://github.com/ndaidong/people-detecting.git
 cd people-detecting
-git checkout BRANCH
+git checkout accumulateWeighted
 
 virtualenv --python=python3 --system-site-packages venv
 source venv/bin/activate
@@ -14,3 +16,14 @@ pip install -r requirements.txt
 
 python start.py
 ```
+
+# How
+
+- Use [cvtColor](https://docs.opencv.org/3.2.0/d7/d1b/group__imgproc__misc.html#ga397ae87e1288a81d2363b61574eb8cab) & [GaussianBlur](https://docs.opencv.org/3.1.0/d4/d86/group__imgproc__filter.html#gaabe8c836e97159a9193fb0b11ac52cf1) to create gray version of frame.
+- Use [accumulateWeighted](https://docs.opencv.org/2.4/modules/imgproc/doc/motion_analysis_and_object_tracking.html#accumulateweighted) to analyze the motions.
+- Use [threshold](https://docs.opencv.org/3.1.0/d7/d1b/group__imgproc__misc.html#gae8a4a146d1ca78c626a53577199e9c57) & [dilate](https://docs.opencv.org/3.1.0/d4/d86/group__imgproc__filter.html#ga4ff0f3318642c4f469d0e11f242f3b6c) to filter.
+- Use [findContours](https://docs.opencv.org/3.1.0/d3/dc0/group__imgproc__shape.html#ga17ed9f5d79ae97bd4c7cf18403e1689a) to estimate the objects. 
+
+
+
+This method is fast, but the accuracy is quite low.
---
+++ start.py
@@ -0,0 +1,53 @@
+import cv2
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+cap = cv2.VideoCapture('vtest.avi')
+
+w = cap.get(3)
+h = cap.get(4)
+mx = int(w - 400)
+my = int(h - 24)
+
+avg = None
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    if ret == False:
+        break
+    
+    k = cv2.waitKey(30) 
+    if k == 27:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    if avg is None:
+        avg = gray.copy().astype("float")
+        continue
+    
+    cv2.accumulateWeighted(gray, avg, 0.1)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+    cv2.imshow('Frame', frameDelta)
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    _, contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    people_detected = 0
+
+    for c in contours:
+        if cv2.contourArea(c) < 4000:
+            continue
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        people_detected += 1
+
+    text = 'People detected: ' + str(people_detected)
+    cv2.putText(frame, text, (mx, my), font, 1, (255, 255, 255), 1, cv2.LINE_AA) 
+    cv2.imshow('Frame',frame)
+
+cap.release()
+cv2.destroyAllWindows()
